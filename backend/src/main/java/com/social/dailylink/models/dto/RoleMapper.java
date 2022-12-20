e5d64ef2a801b9c3fab7e4b37947ceb6387a8212
Sashimi4 <sascha.buthelezi@noseryoung.ch>
2022-12-20 11:40:10 +0100

    Small change: replaced simple string with global text call.

--- backend/src/main/java/com/social/dailylink/models/dto/RoleMapper.java
+++ backend/src/main/java/com/social/dailylink/models/dto/RoleMapper.java
@@ -1,11 +1,12 @@
 package com.social.dailylink.models.dto;
 
 import com.social.dailylink.generic.DTOMapper;
+import com.social.dailylink.global.GlobalStrings;
 import com.social.dailylink.models.Category;
 import com.social.dailylink.models.Role;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = GlobalStrings.MAPPER_COMPONENT_MODEL_NAME, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RoleMapper extends DTOMapper<Role, RoleDTO> {
 }
