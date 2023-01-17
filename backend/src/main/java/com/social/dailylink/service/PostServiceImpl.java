1198888a3d8bf71f78a970023cb824051a0b3f14
davidemarcoli <davide@marcoli.ch>
2023-01-17 10:35:47 +0100

    Optimize imports and overriden methods from PostServiceImpl

--- backend/src/main/java/com/social/dailylink/service/PostServiceImpl.java
+++ backend/src/main/java/com/social/dailylink/service/PostServiceImpl.java
@@ -1,20 +1,14 @@
 package com.social.dailylink.service;
 
-import com.social.dailylink.exception.exitems.EntityAlreadyExistsException;
 import com.social.dailylink.generic.AbstractEntityRepository;
 import com.social.dailylink.generic.AbstractEntityServiceImpl;
 import com.social.dailylink.model.Post;
-import com.social.dailylink.model.User;
 import com.social.dailylink.repository.PostRepository;
 import com.social.dailylink.repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.List;
 
 @Service
 @Transactional
@@ -25,21 +19,6 @@ public class PostServiceImpl extends AbstractEntityServiceImpl<Post> implements 
     public PostServiceImpl(AbstractEntityRepository<Post> repository, UserRepository userRepository) {
         super(repository);
         this.userRepository = userRepository;
-    }
-
-    @Override
-    public Post create(Post entity) {
-        return super.create(entity);
-    }
-
-    @Override
-    public List<Post> findAll() {
-        return super.findAll();
-    }
-
-    @Override
-    public Post updateById(String id, Post entity) throws EntityNotFoundException {
-        return super.updateById(id, entity);
     }
 
     @Override
