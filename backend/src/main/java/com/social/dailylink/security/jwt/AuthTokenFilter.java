e92a50888a5b6a9b11dc68ba5b7235eb46bcbd0f
lazarpe <lazar.petrovic.nefti@gmail.com>
2023-01-20 21:31:06 +0100

    Quick clean up

--- backend/src/main/java/com/social/dailylink/security/jwt/AuthTokenFilter.java
+++ backend/src/main/java/com/social/dailylink/security/jwt/AuthTokenFilter.java
@@ -6,8 +6,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
